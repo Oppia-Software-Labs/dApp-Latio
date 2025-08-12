2470c58df9e115b363de387f8d59b50a2b8ce051
villarley <santivillarley1010@gmail.com>
2025-08-11 19:09:21 -0600

    chore: transpile passkey packages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  reactStrictMode: true,
+  transpilePackages: ["passkey-kit", "passkey-kit-sdk"],
 };
 
 export default nextConfig;
